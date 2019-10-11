6c75ea04f11c01e190fa7584ad27aebf11a5ff41
kenshin.fish <kenshinfish@whatever.com>
2019-10-11 08:54:12 +0530

    Commiting broken adv_tokenize (Yeah Broken)

--- adv_tokenize.py
+++ adv_tokenize.py
@@ -1,33 +1,42 @@
 import sys
 import os
 import re
-import PyPDF2 as pf
+import pdftotext as pt
 import glob
 import pandas as pd
-
-for year in range(2008,2019)[::-1]:
+for year in range(2019,2020)[::-1]:
     dflist = []
-    flist = glob.glob('data/%s*.pdf' % year)
+    flist = glob.glob('text/%s*.txt' % year)
     print("Total :", len(flist))
-    rex = re.compile('.*(hindu|christian|islam).*?to (hindu|christianity|islam).*')
+    rex = re.compile('.*(hindu|christian|christianity|islam).*?to (hindu|christianity|christian|islam).*')
     for i,f in enumerate(flist):
-        print('DEBUG : %s %s' % (i,f))
-        fp = open(f, 'rb')
-        pdf = pf.PdfFileReader(fp)
-        info = pdf.getDocumentInfo()
-        p_num = pdf.getNumPages()
-        pm_list = []
-        for i in range(p_num):
-            text = pdf.getPage(i).extractText()
-            text = " ".join(re.findall('[a-zA-Z]+', text))
-            mlist = rex.findall(text.lower())
-            pm_list += mlist
-        if pm_list != []:
-            df = pd.DataFrame(pm_list, columns=['from','to'])
-            year, week = os.path.basename(f).replace('.pdf','').split('_')
-            df['year'] = int(year)
-            df['week'] = int(week)
-            dflist.append(df)
+        pages = ""
+        with open(f) as fp:
+            pages+=fp.read()
+        lines = pages.split(str(year))
+        for group in lines:
+            if 'convert' in group:
+                x = "".join(group.split('\n')).lower()
+                if 'convert' in x:
+                    rrex = re.compile('.*(hindu|christian|islam).*(hindu|christian|islam).*')
+                    grex = re.compile('.*(daughter|wife).*')
+                    mlist = rrex.search(x)
+                    glist = grex.search(x)
+                    from_rel.append(mlist.group(1) if mlist != None else 'UNK')
+                    to_rel.append(mlist.group(1) if mlist != None else 'UNK')
+                    from_gender.append('F' if glist != None else 'UNK')
+
+                    from_gender.append('UNK')
+                    if rex.search(x) != None:
+                        from_gender = 'F'
+                    else:
+                        from_gender = 'M'
+
+                df = pd.DataFrame({'from':from_rel, 'to':to_rel,'gender':from_gender})
+                year, week = os.path.basename(f).replace('.txt','').split('_')
+                df['year'] = int(year)
+                df['week'] = int(week)
+                dflist.append(df)
 
 
     df = pd.concat(dflist).reset_index(drop=True)
