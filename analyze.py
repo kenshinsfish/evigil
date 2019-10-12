3432c115704a6f4916509247c38d008807d04850
kenshin.fish <kenshinfish@whatever.com>
2019-10-12 17:14:32 +0530

    Updates to analyze scripts using latest data

--- analyze.py
+++ analyze.py
@@ -10,13 +10,13 @@ for f in files:
 df = pd.concat(dflist).reset_index(drop=True)
 dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'islam')]
 print("h->i = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'hindu') & ((df['to'] == 'christianity') | (df['to'] == 'christian'))]
+dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'christian')]
 print("h->c = ",dfhi.shape[0])
 dfhi = df[(df['from'] == 'islam') & (df['to'] == 'hindu')]
 print("i->h = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'islam') & ((df['to'] == 'christianity') | (df['to'] == 'christian'))]
+dfhi = df[(df['from'] == 'islam') & (df['to'] == 'christian')]
 print("i->c = ",dfhi.shape[0])
-dfhi = df[((df['from'] == 'christianity') | (df['from'] == 'christian')) & (df['to'] == 'islam')]
+dfhi = df[(df['from'] == 'christian') & (df['to'] == 'islam')]
 print("c->i = ",dfhi.shape[0])
-dfhi = df[((df['from'] == 'christianity') | (df['from'] == 'christian')) & (df['to'] == 'hindu')]
+dfhi = df[(df['from'] == 'christian') & (df['to'] == 'hindu')]
 print("c->h = ",dfhi.shape[0])
--- time_analyze.py
+++ time_analyze.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import glob
+import sys
 
-files = glob.glob('*.csv')
+files = glob.glob('2*.csv')
 dflist = []
 for f in files:
-    df = pd.read_csv(f, index_col=[0])
+    df = pd.read_csv(f)
     dflist.append(df)
 
 df = pd.concat(dflist).reset_index(drop=True)
-#df['wy'] = df['year'].astype('str') + '-W' + df['week'].astype('str') + '-1'
-#df['date'] = pd.to_datetime(df['wy'],format="%Y-W%W-%w")
-#df.drop('wy',axis=1)
-df.to_csv('y.csv')
+df['wy'] = df['year'].astype('str') + '-W' + df['week'].astype('str') + '-1'
+df['date'] = pd.to_datetime(df['wy'],format="%Y-W%W-%w")
+df.drop('wy',axis=1)
+df.to_csv('y.csv', index=False)
