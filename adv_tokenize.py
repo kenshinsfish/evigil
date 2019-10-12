c074cdd1a5736e3d5290feaccce449f719355f33
kenshin.fish <kenshinfish@whatever.com>
2019-10-12 20:11:35 +0530

    Birthdate entry in database

--- adv_tokenize.py
+++ adv_tokenize.py
@@ -7,6 +7,7 @@ import pandas as pd
 rrex = re.compile('.*(hindu|christian|islam).*(hindu|christian|islam).*')
 grex = re.compile('.*(daughter|wife).*')
 drex = re.compile('.*\(native.*district:(.*)\),.*')
+bdrex = re.compile('.*born on(.*)\(native district:.*')
 for year in range(2008,2020)[::-1]:
     dflist = []
     flist = glob.glob('text/%s*.txt' % year)
@@ -16,24 +17,45 @@ for year in range(2008,2020)[::-1]:
         pages = ""
         with open(f) as fp:
             pages+=fp.read()
+        pages = pages.replace(str(int(year)-1), str(year))
         lines = pages.split(str(year))
 
         from_rel = []
         to_rel = []
         gender = []
         district = []
+        bday = []
         for group in lines:
             if 'convert' in group:
-                x = "".join(group.split('\n')).lower()
+                x = "".join(group.split('\n')).lower() + str(year)
                 mlist = rrex.search(x)
                 glist = grex.search(x)
                 dlist = drex.search(x)
+                bdlist = bdrex.search(x)
                 from_rel.append(mlist.group(1) if mlist != None else 'UNK')
                 to_rel.append(mlist.group(2) if mlist != None else 'UNK')
                 gender.append('F' if glist != None else 'M')
                 district.append(dlist.group(1).strip() if dlist != None else 'UNK')
+                if bdlist != None:
+                    bdstr = bdlist.group(1).split()
+                    if len(bdstr) == 3:
+                        day, month, year = bdstr
+                    elif len(bdstr) == 2:
+                        rex = re.compile('.*([a-z]*)([0-9]*).*')
+                        mobj = rex.search(bdstr[1])
+                        day = bdstr[0]
+                        month = mobj.group(1)
+                        year = mobj.group(2)
+                    else:
+                        print f, group
+                        raise
+                    day = day.strip('stndrh')
+                    bday.append("%s-%s-%s" % (day, month, year))
+                else:
+                    bday.append('UNK')
 
-        df = pd.DataFrame({'from':from_rel, 'to':to_rel,'gender':gender, 'district':district})
+        df = pd.DataFrame({'from':from_rel, 'to':to_rel,'gender':gender,
+                            'district':district, 'bday': bday})
         year, week = os.path.basename(f).replace('.pdf.txt','').split('_')
         df['year'] = int(year)
         df['week'] = int(week)
