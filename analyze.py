27ebbda68eda75b635b914834f200ec3a324fd33
kenshin.fish <kenshinfish@whatever.com>
2019-10-13 13:48:00 +0530

    analyze.py with new data

--- analyze.py
+++ analyze.py
@@ -10,13 +10,13 @@ for f in files:
 df = pd.concat(dflist).reset_index(drop=True)
 dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'islam')]
 print("h->i = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'christian')]
+dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'chris')]
 print("h->c = ",dfhi.shape[0])
 dfhi = df[(df['from'] == 'islam') & (df['to'] == 'hindu')]
 print("i->h = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'islam') & (df['to'] == 'christian')]
+dfhi = df[(df['from'] == 'islam') & (df['to'] == 'chris')]
 print("i->c = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'christian') & (df['to'] == 'islam')]
+dfhi = df[(df['from'] == 'chris') & (df['to'] == 'islam')]
 print("c->i = ",dfhi.shape[0])
-dfhi = df[(df['from'] == 'christian') & (df['to'] == 'hindu')]
+dfhi = df[(df['from'] == 'chris') & (df['to'] == 'hindu')]
 print("c->h = ",dfhi.shape[0])
