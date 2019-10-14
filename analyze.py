3c5fdec68d383f1e098c1562334afc543e5936b8
kenshinsfish <kenshinsfish@whatever.com>
2019-10-14 11:39:28 +0000

    New age field for getting age @ conversion

--- analyze.py
+++ analyze.py
@@ -10,11 +10,17 @@ for f in files:
 df = pd.concat(dflist).reset_index(drop=True)
 df = df[df['cday'] != 'UNK']
 df = df[df['regdate'] != 'UNK']
+df = df[df['bday'] != 'UNK']
 df['regdate'] =  pd.to_datetime(df['regdate'], format='%Y-%m-%d', errors='coerce')
 df['cday'] =  pd.to_datetime(df['cday'], format='%Y-%m-%d', errors='coerce')
+df['bday'] =  pd.to_datetime(df['bday'], format='%Y-%m-%d', errors='coerce')
 df['delay'] = df['regdate'] -  df['cday']
+df['age'] = df['cday'] -  df['bday']
 df = df[df['delay'] >= pd.Timedelta(0)]
+df = df[df['age'] >= pd.Timedelta(0)]
 df['delay'] = df.delay.dt.days
+df['age'] = df.age.dt.days/364.0
+df['age'] = df.age.round().astype(int)
 df.to_csv('gazette_curated.csv')
 dfhi = df[(df['from'] == 'hindu') & (df['to'] == 'islam')]
 print("h->i = ",dfhi.shape[0])
